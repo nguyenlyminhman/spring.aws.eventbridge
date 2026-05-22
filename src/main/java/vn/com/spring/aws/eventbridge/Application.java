4bb9119b98f9504f6e22018de081205abbdec7db
nguyenlyminhman <nguyenlyminhman@gmail.com>
2026-05-22 13:30:26 +0700

    init project

---
+++ src/main/java/vn/com/spring/aws/eventbridge/Application.java
@@ -0,0 +1,13 @@
+package vn.com.spring.aws.eventbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/vn/com/spring/aws/eventbridge/ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.spring.aws.eventbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
